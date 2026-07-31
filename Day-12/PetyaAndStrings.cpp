f636fee16938635907d405e0312c5edbfca05abb
Vinayak Dhyani <vinayakdhyani27@gmail.com>
2026-07-31 12:28:58 -0700

    Add PetyaAndStrings.cpp for string comparison

---
+++ Day-12/PetyaAndStrings.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    // Convert both strings to lowercase
+    for (int i = 0; i < s1.length(); i++)
+        s1[i] = tolower(s1[i]);
+
+    for (int i = 0; i < s2.length(); i++)
+        s2[i] = tolower(s2[i]);
+
+    // Compare
+    if (s1 < s2)
+        cout << -1;
+    else if (s1 > s2)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
