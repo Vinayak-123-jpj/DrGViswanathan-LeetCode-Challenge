b3b01fbccb6987b23104a8a4ef1c379fa9ef9bf9
Vinayak Dhyani <vinayakdhyani27@gmail.com>
2026-08-17 20:51:48 +0530

    Implement magnet group counting program
    This program counts the number of groups of magnets based on user input.

---
+++ Day-31/Magnets.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string prev, current;
+    cin >> prev;
+
+    int groups = 1;
+
+    for (int i = 1; i < n; i++) {
+        cin >> current;
+
+        if (current != prev) {
+            groups++;
+        }
+
+        prev = current;
+    }
+
+    cout << groups << endl;
+
+    return 0;
+}
