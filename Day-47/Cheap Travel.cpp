0d6620a209b632bd0c314d29a6ec711ffd04b76a
Vinayak Dhyani <vinayakdhyani27@gmail.com>
2026-09-06 16:04:35 +0530

    Add Cheap Travel program for cost calculation

---
+++ Day-47/Cheap Travel.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+
+    int ans = min(n * a, (n / m) * b + min((n % m) * a, b));
+
+    cout << ans;
+}
